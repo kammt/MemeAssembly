9508c09c9a156d9be439f38de7ca3751156a03b5
tobias <tobias.kamm@pm.me>
2021-03-11 16:53:48 +0100

    Removed terminal color values

--- compiler/translate.c
+++ compiler/translate.c
@@ -5,15 +5,6 @@
 
 #include "log.h" //Printing to the console
 
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
-#define RESET "\x1B[0m"
-
 /**
  * translate.c:
  * This file concerns the translation of MemeASM code into x86-Assembly. It uses an array of commandPatterns and their respective TranslationPatterns, which are defined below
