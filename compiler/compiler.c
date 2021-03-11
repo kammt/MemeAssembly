b9c6abbfae7ffdebe7d183ee8da5f917b587649c
tobias <tobias.kamm@pm.me>
2021-03-11 16:22:42 +0100

    compiler.c now fully relies on log.c

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -5,18 +5,10 @@
 
 #include "translate.h" //Translation to Assembly
 #include "analyse.h" //Semantic analysis
+#include "log.h" //Semantic analysis
 
 #include <sys/types.h>
 #include <unistd.h>
-
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
-#define RESET "\x1B[0m"
 
 /**
  * compiler.c:
@@ -30,11 +22,12 @@
  * @param destPTR a pointer to the destination file. If nonexistent, it will be created
  */
 void compile(FILE *srcPTR, FILE *destPTR) {
+    printInfoMessage("Starting Assembly-Translation...");
     startTranslation(srcPTR, destPTR);
+    printInfoMessage("Starting semantic analysis...");
     startSemanticAnalysis();
 
-    printf(GRN "File compiled successfully!" RESET);
-    printf("\n");
+    printSuccessMessage("File compiled successfully!");
 }
 
 /**
@@ -45,16 +38,13 @@ void compileAndRun(FILE *srcPTR) {
     FILE *destPTR = fopen("tmp.asm","w");
     compile(srcPTR, destPTR);
 
-    printf(YEL "Calling nasm...\n" RESET);
-    fflush( stdout );
+    printStatusMessage("Calling nasm...");
     system("nasm -f elf32 -o tmp.o tmp.asm");
 
-    printf(YEL "Linking...\n" RESET);
-    fflush( stdout );
+    printStatusMessage("Linking...");
     system("gcc tmp.o -g -o tmp -m32 -fno-pie -no-pie");
     
-    printf(YEL "Running file...\n" RESET);
-    fflush( stdout );
+    printStatusMessage("Running file...");
     execv ("tmp", NULL);
     //TODO delete temporary files
 }
