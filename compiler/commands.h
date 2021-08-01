2f225adfd8fb3e439e0605f7514c0d297b1e523d
tobias <tobias.kamm@pm.me>
2021-08-01 23:57:32 +0200

    Added the option to save a char-Pointer instead of two parameters

--- compiler/commands.h
+++ compiler/commands.h
@@ -3,22 +3,34 @@
 
 #include <stdint.h>
 
-#define NUMBER_OF_COMMANDS 28
-#define COMMAND_LIST_MAX_STRING_LENGTH 66
+#define NUMBER_OF_COMMANDS 29
+#define COMMAND_PATTERN_LIST_MAX_STRING_LENGTH 66
+#define COMMAND_TRANSLATION_LIST_MAX_STRING_LENGTH 33
 #define MAX_PARAMETER_COUNT 2
 #define MAX_PARAMETER_LENGTH 4
 
 #define OR_DRAW_25_OPCODE NUMBER_OF_COMMANDS - 1;
 
-struct command {
-    uint8_t opcode;
+struct params {
     char params[MAX_PARAMETER_COUNT][MAX_PARAMETER_LENGTH];
+};
+
+struct variablePointer {
+    char* param;
+};
+
+struct parsedCommand {
+    uint8_t opcode;
+    union {
+        struct params paramsArray;
+        struct variablePointer pointer;
+    } parameters;
     uint8_t isPointer; //0 = No Pointer, 1 = first parameter, 2 = second parameter
     int lineNum;
 };
 
 struct commandsArray {
-    struct command* arrayPointer;
+    struct parsedCommand* arrayPointer;
     int size;
 };
 
