f1702a3d7282e83bd4b40fe879f26549e852c446
tobias <tobias.kamm@pm.me>
2021-08-03 16:39:41 +0200

    Implemented parameter parsing

--- compiler/analyzer/parameters.h
+++ compiler/analyzer/parameters.h
@@ -1,4 +1,8 @@
 #ifndef MEMEASSEMBLY_PARAMETERS_H
 #define MEMEASSEMBLY_PARAMETERS_H
 
+#include "../commands.h"
+
+int hasValidParameters(struct parsedCommand parsedCommand);
+
 #endif //MEMEASSEMBLY_PARAMETERS_H
