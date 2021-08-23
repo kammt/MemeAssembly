674cf50d5a8ce727b7e8c44d87fdef14576d2e2d
tobias <tobias.kamm@pm.me>
2021-08-23 11:20:50 +0000

    Added license/copyright notices

--- compiler/logger/log.c
+++ compiler/logger/log.c
@@ -1,5 +1,23 @@
-#include <stdio.h>  //Printf() function
-#include <stdlib.h> //Exit() function
+/*
+This file is part of the MemeAssembly compiler.
+
+ Copyright © 2021 Tobias Kamm
+
+MemeAssembly is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MemeAssembly is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MemeAssembly. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
 
 #define RED   "\x1B[31m"
 #define GRN   "\x1B[32m"
