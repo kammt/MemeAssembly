df1b65eae408d1e147a486c60a5ca9f73fcf2f0c
tobias <tobias.kamm@pm.me>
2021-08-23 13:18:25 +0000

    Also writing function end label if it is the last function in the file

--- compiler/translator/translator.c
+++ compiler/translator/translator.c
@@ -206,7 +206,7 @@ void translateToAssembly(struct commandsArray *commandsArray, size_t index, FILE
 
     if(useStabs && parsedCommand.opcode != 0) {
         //If this was a return statement and this is the end of file or a function definition is followed by it, we reached the end of the function. Define the label for the N_RBRAC stab
-        if(parsedCommand.opcode > 0 && parsedCommand.opcode <= 3 && commandsArray -> arrayPointer[index + 1].opcode == 0) {
+        if(parsedCommand.opcode > 0 && parsedCommand.opcode <= 3 && (commandsArray -> size == index + 1 || commandsArray -> arrayPointer[index + 1].opcode == 0)) {
             stabs_writeFunctionEndLabel(outputFile);
         }
         //In any case, we now need to write the line info to the file
