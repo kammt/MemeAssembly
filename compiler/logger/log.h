0816e55f46dfec52912d5b228773bb0c1dc2d1f0
tobias <tobias.kamm@pm.me>
2022-06-07 15:51:14 +0200

    log.h: Removed colors in error messages

--- compiler/logger/log.h
+++ compiler/logger/log.h
@@ -23,14 +23,14 @@ along with MemeAssembly. If not, see <https://www.gnu.org/licenses/>.
 #include <stdio.h>  //Printf() function
 #include "../compiler.h"
 
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
-#define RESET "\x1B[0m"
+#define RED   ""
+#define GRN   ""
+#define YEL   ""
+#define BLU   ""
+#define MAG   ""
+#define CYN   ""
+#define WHT   ""
+#define RESET ""
 
 void printInformationHeader();
 
